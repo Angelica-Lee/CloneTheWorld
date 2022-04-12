59e392c940c6d8bab217dd6bcd96f3c10a27ed76
zhiqi <zhiqili@andrew.cmu.edu>
2022-04-12 02:14:50 -0400

    if user donot show name

--- messenger/src/Message.js
+++ messenger/src/Message.js
@@ -1,20 +1,21 @@
 import { Card, CardContent, Typography } from '@material-ui/core'
-import React from 'react'
+import React, {forwardRef} from 'react'
 import './Message.css'
 
-function Message({message, username}) {
+const Message = React.forwardRef(({message, username}, ref) => {
   const isUser = username === message.username;
   return (
-    <div className={`message ${isUser && 'message__user'}`}>
+    <div ref={ref} className={`message ${isUser && 'message__user'}`}>
       <Card className={isUser? "message__userCard":"message__guestCard"}>
       <CardContent>
-        <Typography  color="blue" variant="h5" component="h2">
-          {message.username} : {message.message}
+        <Typography  variant="h5" component="h2">
+          {!isUser && `${message.username} :`} {message.message}
         </Typography>
       </CardContent>
     </Card>
     </div>
   )
 }
+)
 
 export default Message
